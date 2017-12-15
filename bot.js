55542b2065f6e197a9a255b5f5cf6ff7ae260cc6
KaiJames <34582509+KaiJames@users.noreply.github.com>
2017-12-15 20:03:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,28 @@ client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
+    
+    });
+client.on('message',message => {
+	if(message.author.bot){ return;}
+	console.log('Message from ' + message.author.username + '#'+ message.author.discriminator + '\n Saying: ' + message.content);
+	if(message.content == '!list') {
+		message.channel.sendMessage('---------------- Commands List ----------------');
+		message.channel.sendMessage('!stats -- Show current radio stats.');
+		sleep(500);
+		message.channel.sendMessage('!list -- Shows this message.');
+		message.channel.sendMessage('!about -- Information of BOT.');
+		sleep(500);
+		message.channel.sendMessage('!panel -- Panel link');
+		sleep(1000);
+		message.channel.sendMessage('!Kai -- Information about Kai');
+		message.channel.sendMessage('!Beats -- Information about Codi');
+		message.channel.sendMessage('!Codi -- Information about Codi');
+		sleep(500);
+	}
 });
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
